1324c2a6b60c887ee256bcf2fb115c03d1de56af
Brandon Karchewski <brandon.karchewski@ucalgary.ca>
2025-01-14 08:35:07 -0700

    made some demo Python code files and test script

---
+++ examples/driver.py
@@ -0,0 +1,16 @@
+from goph420_lab00.functions import (
+    add,
+    multiply,
+)
+
+
+def main():
+    x = 5
+    y = 22
+
+    print(f"add(x: {x}, y: {y}): {add(x, y)}")
+    print(f"multiply(x: {x}, y: {y}): {multiply(x, y)}")
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/goph420_lab00/functions.py
@@ -0,0 +1,8 @@
+def add(x, y):
+    """Add two numbers."""
+    return x + y
+
+
+def multiply(x, y):
+    """Multiply two numbers."""
+    return x * y
