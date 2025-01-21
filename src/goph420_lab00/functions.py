83d259fa5affcd1927646bc2e265c9c47ef6859b
Brandon Karchewski <brandon.karchewski@ucalgary.ca>
2025-01-21 08:38:42 -0700

    added subtract function

--- src/goph420_lab00/functions.py
+++ src/goph420_lab00/functions.py
@@ -3,6 +3,11 @@ def add(x, y):
     return x + y
 
 
+def subtract(x, y):
+    """Subtract two numbers."""
+    return x - y
+
+
 def multiply(x, y):
     """Multiply two numbers."""
     return x * y
